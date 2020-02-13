e8656ed4f17ca9613189e7ac3f5d80bccf9bf49d
Nick Mc <nbmc@protonmail.com>
2020-02-13 10:00:04 -0600

    rewrote async block to properly implement await WITHOUT then.

--- scripts.js
+++ scripts.js
@@ -33,32 +33,38 @@
 //     console.log(v);
 // }
 
-// async/await.
+// async/await demonstration.
+// define an async function, comprised of a try/catch block; all awaits contained in the try and executed sequentially.
+// even though the try executes 'synchronously' it is still asynchronous because each waits on the previous one.
+// also results in greater economy of code: one catch for all await promises.
+let asyncDemo = async () => {
+    try {
+        let value = await slowMath.add(6, 2);
+        say(value);
+        value = await slowMath.multiply(value, 2);
+        say(value);
+        value = await slowMath.divide(value, 4);
+        say(value);
+        value = await slowMath.subtract(value, 3);
+        say(value);
+        value = await slowMath.add(value, 98);
+        say(value);
+        value = await slowMath.remainder(value, 2);
+        say(value);
+        value = await slowMath.multiply(value, 50);
+        say(value);
+        value = await slowMath.remainder(value, 40);
+        say(value);
+        value = await slowMath.add(value, 32);
+        say(`The final result is: ${value}`);
+    }
+    catch {
+        say(new Error('Something went terribly wrong!'));
+    }
+}
 function say(v) {
     let div = document.createElement('div');
     div.innerText = v;
     document.body.appendChild(div);
 }
-let v;
-let val = async () => { v = await slowMath.add(6, 2); say(v); };
-val().then(
-    val = async () => { v = await slowMath.multiply(v, 2); say(v); }
-).then(
-    val = async () => { v = await slowMath.divide(v, 4); say(v); }
-).then(
-    val = async () => { v = await slowMath.subtract(v, 3); say(v); }
-).then(
-    val = async () => { v = await slowMath.add(v, 98); say(v); }
-).then(
-    val = async () => { v = await slowMath.remainder(v, 2); say(v); }
-).then(
-    val = async () => { v = await slowMath.multiply(v, 50); say(v); }
-).then(
-    val = async () => { v = await slowMath.remainder(v, 40); say(v); }
-).then(
-    val = async () => { v = await slowMath.add(v, 32); }
-).then(
-    async () => { say("The final result is: " + v) }
-).catch(
-    async () => { say(new Error('Something went terribly wrong!'))}
-);
+asyncDemo();
